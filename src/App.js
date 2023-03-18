1e7ddd33453e15a5c1b62bea0b81950fedb3fa6f
zarinadev <zarina.mendybay@gmail.com>
2023-03-18 14:13:20 -0400

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Square from "./Components/Square";
+import { Patterns } from "./Patterns";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("O");
+  const [result, setResult] = useState({ winner: "none", state: "none" });
 
-export default App;
+  useEffect(() => {
+    checkWin();
+    checkIfTie();
+
+    if (player == "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  }, [board]);
+
+  useEffect(() => {
+    if (result.state != "none") {
+      alert(`Game Finished! Winning Player: ${result.winner}`);
+      restartGame();
+    }
+  }, [result]);
+
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((value, idx) => {
+        if (idx == square && value == "") {
+          return player;
+        }
+
+        return value;
+      })
+    );
+  };
+
+  const checkWin = () => {
+    Patterns.forEach((currPattern) => {
+      const firstPlayer = board[currPattern[0]];
+      if (firstPlayer == "") return;
+      let foundWinningPattern = true;
+      currPattern.forEach((idx) => {
+        if (board[idx] != firstPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "Won" });
+      }
+    });
+  };
+
+  const checkIfTie = () => {
+    let filled = true;
+    board.forEach((square) => {
+      if (square == "") {
+        filled = false;
+      }
+    });
+
+    if (filled) {
+      setResult({ winner: "No One", state: "Tie" });
+    }
+  };
+
+  const restartGame = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setPlayer("O");
+  };
+
+    return (
+      <div className="App">
+        <div className="board">
+          <div className="row">
+            <Square
+              value={board[0]}
+              chooseSquare={() => {
+                chooseSquare(0);
+              }}
+            />
+            <Square
+              value={board[1]}
+              chooseSquare={() => {
+                chooseSquare(1);
+              }}
+            />
+            <Square
+              value={board[2]}
+              chooseSquare={() => {
+                chooseSquare(2);
+              }}
+            />
+          </div>
+          <div className="row">
+            <Square
+              value={board[3]}
+              chooseSquare={() => {
+                chooseSquare(3);
+              }}
+            />
+            <Square
+              value={board[4]}
+              chooseSquare={() => {
+                chooseSquare(4);
+              }}
+            />
+            <Square
+              value={board[5]}
+              chooseSquare={() => {
+                chooseSquare(5);
+              }}
+            />
+          </div>
+          <div className="row">
+            <Square
+              value={board[6]}
+              chooseSquare={() => {
+                chooseSquare(6);
+              }}
+            />
+            <Square
+              value={board[7]}
+              chooseSquare={() => {
+                chooseSquare(7);
+              }}
+            />
+            <Square
+              value={board[8]}
+              chooseSquare={() => {
+                chooseSquare(8);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
